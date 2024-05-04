dfa7e8fa6e9e5c8a77a9c8a9d235696cc5d4b2a2
joxrfc <jorgefcea@gmail.com>
2024-05-04 02:53:58 +0200

    Posts y Comments

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import "./comments.scss";
+import { AuthContext } from "../../context/authContext";
+
+const Comments = () => {
+  const { currentUser } = useContext(AuthContext);
+  //Temporary
+  const comments = [
+    {
+      id: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+      name: "Rubén de la Blanca",
+      userId: 1,
+      profilePicture:
+        "https://images.unsplash.com/photo-1536164261511-3a17e671d380?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+      name: "David Takeli",
+      userId: 2,
+      profilePicture:
+        "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+  return (
+    <div className="comments">
+      <div className="write">
+        <img src={currentUser.profilePic} alt="" />
+        <input type="text" placeholder="Publica un comentario" />
+        <button>Send</button>
+      </div>
+      {comments.map((comment) => (
+        <div className="comment">
+          <img src={comment.profilePicture} alt="" />
+          <div className="info">
+            <span>{comment.name}</span>
+            <p>{comment.desc}</p>
+          </div>
+          <span className="date">hace 1 hora</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Comments;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,11 +1,33 @@
+import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-    return (
-        <div className="posts">
-            Posts
-        </div>
-    );
-}
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "David Fernández",
+      userId: 1,
+      profilePic:
+        "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.unsplash.com/photo-1551632811-561732d1e306?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      name: "Sergio Takeli",
+      userId: 2,
+      profilePic:
+        "https://images.unsplash.com/photo-1505628346881-b72b27e84530?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+  ];
+
+  return <div className="posts">
+    {posts.map(post=>(
+      <Post post={post} key={post.id}/>
+    ))}
+  </div>;
+};
 
 export default Posts;
